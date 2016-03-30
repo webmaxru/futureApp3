18a3b8c5cb4d9247695d966eb07288899ec3c430
Maxim Salnikov <salnikov@gmail.com>
2016-03-30 11:08:55 +0200

    switched to TS

---
+++ app/pages/page1/page1.ts
@@ -0,0 +1,37 @@
+import {Page} from 'ionic-angular';
+import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
+import {Observable} from 'rxjs/Observable';
+
+import {FirebaseRef} from 'angularfire2';
+import {Inject} from 'angular2/core';
+
+@Page({
+  templateUrl: 'build/pages/page1/page1.html',
+  providers: [FIREBASE_PROVIDERS, defaultFirebase('https://futureapp-prague.firebaseio.com/posts/')]
+})
+export class Page1 {
+  questions: Observable<any>;
+  postsRef: Firebase;
+
+  constructor(af: AngularFire, @Inject(FirebaseRef) ref:Firebase) {
+    this.questions = af.list('');
+    this.postsRef = ref;
+  }
+
+  onKeyUp($event, nameInput, messageInput) {
+    if ($event.which === 13 && messageInput.value) {
+      this.createPost(nameInput, messageInput);
+    }
+  };
+
+  createPost(nameInput, messageInput) {
+
+    this.postsRef.push({
+      name: nameInput.value,
+      message: messageInput.value
+    });
+
+    messageInput.value = null;
+  };
+
+}
