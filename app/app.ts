097db3539265640e5f520b4c2ea400b35c0c96c9
Maxim Salnikov <salnikov@gmail.com>
2016-03-30 11:31:18 +0200

    firebase pipe added

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,8 @@ import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
 
+import {enableProdMode} from 'angular2/core';
+enableProdMode();
 
 @App({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
--- app/pages/page2/page2.ts
+++ app/pages/page2/page2.ts
@@ -1,7 +1,18 @@
 import {Page} from 'ionic-angular';
-
+import {FirebaseEventPipe} from '../../firebasepipe/firebasepipe';
 
 @Page({
   templateUrl: 'build/pages/page2/page2.html',
+  pipes: [FirebaseEventPipe]
 })
-export class Page2 {}
+
+
+export class Page2 {
+
+  private firebaseUrl: string;
+
+  constructor() {
+    this.firebaseUrl = 'https://futureapp-prague.firebaseio.com/posts';
+  }
+
+}
